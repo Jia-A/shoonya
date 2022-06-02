8e087dfa3f49ef51010b046ab060cc2da24ac56c
Jia-A <agrawaljiya2011@gmail.com>
2022-06-03 02:11:13 +0530

    feat : static-landing-page-added

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,12 +1,13 @@
 import "../styles/landing.css"
+import "../root.css"
 const Landing = () =>{
     return (
         <div className="App">
             <div className="body-cont">
                 <img src="images/universe-g276597298_1920.jpg" alt="main-page-img" className="hero-img" />
                 <div className="content-box">
-                    <h1 className="welcome-note">Welcome</h1>
-                    <button className="landing-btn">Explore</button>
+                    <h1 className="welcome-note">Welcome to Shoonya</h1>
+                    <button className="landing-btn">Explore...</button>
                 </div>
             </div>
         </div>
