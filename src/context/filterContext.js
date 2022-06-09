0706f7379a814e4970b4d3acc3de815e0a96f640
Jia-A <agrawaljiya2011@gmail.com>
2022-06-09 20:43:47 +0530

    feat : filter-on-categories-added

---
+++ src/context/filterContext.js
@@ -0,0 +1,65 @@
+import { createContext, useReducer, useState, useContext } from "react";
+import { useVideo } from "./videoContext";
+
+const FilterContext = createContext();
+
+const FilterProvider = ({children}) =>{
+    const { videoState } = useVideo();
+    const { videos } = videoState;
+    const filterReducer = (filterState, action) =>{
+        switch(action.type){
+            case "CATEGORY" : 
+            return {
+                ...filterState,
+                filterByCategory : action.payload,
+            }
+            
+            case "CLEAR_FILTER" : 
+            return {
+                ...filterState,
+                filterByCategory : "",
+                allVideos : filterState.allVideos
+            }
+
+            default :
+            return filterState
+        }
+    }
+
+    const [filterState, filterDispatch] = useReducer(filterReducer, {
+        filterByCategory : "",
+        allVideos : [...videos]
+    })
+
+
+
+    const categoryFilter = (videos, filterOnCategory) =>{
+        console.log("in category context")
+        if(videos != [] || videos != "undefined"){
+            const showVideos = [...videos];
+            if (filterOnCategory != "") {
+                const arr = showVideos.filter(
+                    (item) => item.category === filterOnCategory)
+                return arr;
+            }
+            return showVideos;
+        }
+    }
+
+    const filteredVideos = (videos, filterState) =>{
+        console.log("in return filter")
+        
+        const { filterByCategory } = filterState;
+        console.log(filterByCategory)
+        const filterCat = categoryFilter(videos, filterByCategory)
+        return filterCat;
+    }
+
+    return (
+        <FilterContext.Provider value={{filterState, filterDispatch, filteredVideos}}>{children}</FilterContext.Provider>
+    );
+}
+
+const useFilter = () => useContext(FilterContext)
+
+export { FilterProvider, useFilter}
